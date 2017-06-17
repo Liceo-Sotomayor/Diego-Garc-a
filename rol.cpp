62d827bde405196ab1dc3994366d9616d3eaa7ca
Diego-Garc-a <garciagames61@gmail.com>
2017-06-17 11:20:37 -0400

    Se arregla identación.

--- rol.cpp
+++ rol.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 using namespace std;
 int main() {
-    srand(time(NULL));
+  srand(time(NULL));
   bool esMayor, esIgual;  
   int a, b;
   a = rand()%4;
@@ -12,13 +12,13 @@ int main() {
   esMayor = a > b;
   esIgual = a == b;
   if (esIgual){ 
-  cout << a << " es igual a " << b << endl;
+    cout << a << " es igual a " << b << endl;
   } else {
-  if (esMayor) {
-    cout << a << " es mayor que " << b << endl;
-  } else {
-    cout << a << " es menor que " << b << endl;
-  }
+    if (esMayor) {
+        cout << a << " es mayor que " << b << endl;
+    } else {
+        cout << a << " es menor que " << b << endl;
+    }
   }
      
 }
